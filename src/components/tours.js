05bf660f50c1e1ff27a49e4ebb7db2e0a580bf2f
sksunariya <sksunariya0099@gmail.com>
2023-09-02 02:38:06 +0530

    lastcommit

--- src/components/tours.js
+++ src/components/tours.js
@@ -5,7 +5,7 @@ function Tours( {tours, removeTour} ){
     return (
         <div className='font-poppins pb-4 flex flex-col gap-10 justify-center items-center'>
             <div className=''>
-                <h1 className='tracking-wide text-center text-4xl font-semibold mt-10 px-10 py-4 border-[5px] border-blue-950 border-dashed rounded-xl'>Plan Your Tour</h1>
+                <h1 className='tracking-wide text-center text-2xl lg:text-4xl font-semibold mt-10 px-10 py-4 border-[5px] border-blue-950 border-dashed rounded-xl'>Plan Your Tour</h1>
             </div>
             <div className='w-11/12 max-w-[1080px] h-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
